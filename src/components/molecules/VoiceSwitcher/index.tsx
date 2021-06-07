542620604f2c8e0241870ffc6f1c96324db1fe50
Kento Chiba <c.kento.chiba@gmail.com>
2021-06-08 01:56:09 +0900

    Create index.tsx

---
+++ src/components/molecules/VoiceSwitcher/index.tsx
@@ -0,0 +1,11 @@
+import { Select } from '@chakra-ui/react'
+
+const VoiceSwitcher: React.FC = () => {
+  return (
+    <Select>
+      <option value="">value</option>
+    </Select>
+  )
+}
+
+export default VoiceSwitcher
